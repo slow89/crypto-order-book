fa6d6099e40de8e3245a43f2da153dd583762b40
Stephen Lowinger <slowinger@uchicago.edu>
2019-01-08 20:57:27 -0600

    removed unused dependency

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,10 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import composeEnhancers from "./middleware/composeEnhancers";
-import reduxThunk from "redux-thunk";
 import store from "./index";
 import actionMiddleware from "./middleware/actionMiddleware";
 
-const middleware = [reduxThunk, actionMiddleware];
+const middleware = [actionMiddleware];
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
 export default () => createStore(store, enhancer);
