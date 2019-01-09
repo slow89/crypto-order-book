7a98131f6a6b0e6e56de23ddc23c5d1cf3c88ab8
Stephen Lowinger <slowinger@uchicago.edu>
2019-01-08 21:09:48 -0600

    Updates

---
+++ precache-manifest.21cc0f456896d7ef26f9c53561afc92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11916d603d6bb103cbbc",
+    "url": "/crypto-order-book/static/css/main.db9f2636.chunk.css"
+  },
+  {
+    "revision": "11916d603d6bb103cbbc",
+    "url": "/crypto-order-book/static/js/main.11916d60.chunk.js"
+  },
+  {
+    "revision": "1e7a82d0965d0443de15",
+    "url": "/crypto-order-book/static/js/1.1e7a82d0.chunk.js"
+  },
+  {
+    "revision": "8db1895bff0b000b9e5b",
+    "url": "/crypto-order-book/static/js/runtime~main.8db1895b.js"
+  },
+  {
+    "revision": "e8c1ed15e7caf6bbe49b3a4eea10628f",
+    "url": "/crypto-order-book/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/crypto-order-book/precache-manifest.21cc0f456896d7ef26f9c53561afc92e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/crypto-order-book/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
